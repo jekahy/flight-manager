53cbc7d65c40f2afa65dc7f68b823bb8cf393089
Mac <mahmood1@Mahmood1s-Mac.local>
2012-02-18 17:00:47 +0100

    Initial Commit

---
+++ flight manager/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  flight manager
+//
+//  Created by Жека Г on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
